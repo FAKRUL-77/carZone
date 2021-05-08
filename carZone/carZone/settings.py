9e0c113bbc26ffa5ac2dcf33e1aab400cc4514d0
FAKRUL-77 <fakrul15-1316@diu.edu.bd>
2021-05-08 15:23:50 +0600

    [ADDED] Added postgresql settings

--- carZone/carZone/settings.py
+++ carZone/carZone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carZone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'carZone_db',
+        'USER': 'postgres',
+        'PASSWORD': '0xvaxxwk',
+        'HOST': 'localhost',
     }
 }
 
