de7acaad1328ca1d355c79ea100d9e53d7911822
FAKRUL-77 <fakrul15-1316@diu.edu.bd>
2021-05-15 00:22:00 +0600

    [ADDED] - added car-detail page         - added cars page         - edited home page         - set-up urls for car-detail page         - create view for car page and car-detail page

--- carZone/cars/urls.py
+++ carZone/cars/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from cars import views
 urlpatterns = [
     path('', views.cars, name='cars'),
+    path('<int:id>', views.car_detail, name='car_detail'),
 ]
--- carZone/cars/views.py
+++ carZone/cars/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
+from cars.models import Car
 
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.order_by('created_date')
+
+    data = {
+        'cars': cars,
+    }
+    return render(request, 'cars/cars.html', data)
+
+
+def car_detail(request, id):
+    single_car_detail = get_object_or_404(Car, pk=id)
+    data = {
+        'single_car_detail': single_car_detail,
+    }
+    return render(request, 'cars/car_detail.html', data)
